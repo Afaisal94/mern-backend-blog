eef1bd5c99e89035934ef808054d29087223343d
A Faisal <52657193+Afaisal94@users.noreply.github.com>
2022-11-05 11:06:09 +0700

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -6,6 +6,7 @@ const Post = require("../controllers/Post");
 
 router.get("/", Post.getPosts);
 router.get("/:id", Post.getPostById);
+router.get("/categoryname/:name", Post.getPostByCategoryName);
 router.post("/", upload.single("image"), Post.savePost);
 router.put("/:id", upload.single("image"), Post.updatePost);
 router.patch("/:id", upload.single("image"), Post.updatePost);
